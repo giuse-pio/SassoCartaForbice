291128f8b9b30e16045b138374f87d6fd5a698bd
giuse-pio <giupeppepio7@gmail.com>
2024-11-03 22:58:47 +0100

    correzione

--- main.c
+++ main.c
@@ -8,13 +8,26 @@ int main(void) {
     int sceltaNPC, sceltaGiocatore;
     int vittorieNPC = 0, vittoreGiocatore = 0;
     printf("inserisci |0| per indicare -Sasso- "
-           "|1| per indicare -Carta- "
-           "|2| per indicare -Forbici-:\n");
-    printf("per smettere di giocare digita |-1|\n" );
+        "|5| per indicare -Carta- "
+        "|2| per indicare -Forbici-:\n");
+    printf("per smettere di giocare digita |-1|\n");
     do {
         srand(time(NULL));
         sceltaNPC = rand() % (max - min + 1) + min;
         switch (sceltaNPC) {
+            case 1:
+                sceltaNPC = 5;
+            //carta
+                break;
+            case 2:
+                sceltaNPC = 2;
+            //forbici
+                break;
+            default:
+                sceltaNPC = 0;
+            // sasso
+        }
+        switch (sceltaGiocatore) {
             case 1:
                 sceltaNPC = 5;
             //carta
@@ -33,25 +46,46 @@ int main(void) {
         if (sceltaGiocatore == -1) {
             break;
         }
-        if (sceltaGiocatore < min || sceltaGiocatore > max) {
+        if (sceltaGiocatore == 0 || sceltaGiocatore == 2 || sceltaGiocatore == 5) {
+        } else {
             printf("la mossa inserita non esiste riprova \n");
         }
         if (sceltaGiocatore == 0 && sceltaNPC == 2) {
             printf("hai vinto! \n");
             printf("la mossa era %d\n", sceltaNPC);
             vittoreGiocatore++;
+        } else if (sceltaGiocatore == 0 && sceltaNPC == 5) {
+            printf("hai perso! \n");
+            printf("la mossa era %d\n", sceltaNPC);
+            vittoreGiocatore++;
+        } else if (sceltaGiocatore == 0 && sceltaNPC == 0) {
+            printf("pareggio! \n");
+            printf("la mossa era %d\n", sceltaNPC);
+            vittoreGiocatore++;
         } else if (sceltaGiocatore == 2 && sceltaNPC == 5) {
             printf("hai vinto! \n");
             printf("la mossa era %d\n", sceltaNPC);
             vittoreGiocatore++;
-        } else if (sceltaGiocatore == 1 && sceltaNPC == 0) {
+        } else if (sceltaGiocatore == 2 && sceltaNPC == 0) {
+            printf("hai perso! \n");
+            printf("la mossa era %d\n", sceltaNPC);
+            vittoreGiocatore++;
+        } else if (sceltaGiocatore == 2 && sceltaNPC == 2) {
+            printf("pareggio! \n");
+            printf("la mossa era %d\n", sceltaNPC);
+            vittoreGiocatore++;
+        } else if (sceltaGiocatore == 5 && sceltaNPC == 0) {
             printf("hai vinto! \n");
             printf("la mossa era %d\n", sceltaNPC);
             vittoreGiocatore++;
-        } else {
-            printf("hai perso! \n");
+        } else if (sceltaGiocatore == 5 && sceltaNPC == 5) {
+            printf("pareggio! \n");
             printf("la mossa era %d\n", sceltaNPC);
-            vittorieNPC++;
+            vittoreGiocatore++;
+        } else if (sceltaGiocatore == 5 && sceltaNPC == 2) {
+            printf("hai vinto! \n");
+            printf("la mossa era %d\n", sceltaNPC);
+            vittoreGiocatore++;
         }
     } while (sceltaGiocatore != -1);
     printf("il giocatore ha vinto %d volte\n", vittoreGiocatore);
